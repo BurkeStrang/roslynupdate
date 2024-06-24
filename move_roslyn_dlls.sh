77e62e2b0758a0e2830fe19854dc8b358ecc0e05
Burke.Strang <burke.strang@vertigis.com>
2024-06-24 12:11:40 -0400

    adding comments since dosen't really work

--- move_roslyn_dlls.sh
+++ move_roslyn_dlls.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-# before you run this run dotnet-outdated
-# if there are any updates run dotnet-outdated -u
+# run dotnet add package Microsoft.CodeAnalysis.LanguageServer.Linux-x64 --prerelease
+# it will error but the package will be downloaded to ~/.nuget/packages/microsoft.codeanalysis.languageserver.linux-x64
+# do the same thing but with the windows architecture to get the dlls
 
 # Define the base paths
 NUGET_PATH="$HOME/.nuget/packages/microsoft.codeanalysis.languageserver.linux-x64"
@@ -15,9 +16,9 @@ LATEST_VERSION=$(ls -v $NUGET_PATH | tail -n 1)
 
 # Check if the latest version directory exists
 if [ -d "$NUGET_PATH/$LATEST_VERSION" ]; then
-	# Copy the contents of the latest version to the NVIM_ROSLYN_PATH directory
-	cp -r "$NUGET_PATH/$LATEST_VERSION/content/LanguageServer/linux-x64/"* "$NVIM_ROSLYN_PATH/"
-	echo "Copied contents of version $LATEST_VERSION to $NVIM_ROSLYN_PATH"
+        # Copy the contents of the latest version to the NVIM_ROSLYN_PATH directory
+        cp -r "$NUGET_PATH/$LATEST_VERSION/content/LanguageServer/linux-x64/"* "$NVIM_ROSLYN_PATH/"
+        echo "Copied contents of version $LATEST_VERSION to $NVIM_ROSLYN_PATH"
 else
-	echo "Latest version directory not found!"
+        echo "Latest version directory not found!"
 fi
