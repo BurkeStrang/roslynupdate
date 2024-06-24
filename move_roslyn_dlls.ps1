2f3b6ba219ecf6ca96b7382ab3410d4d5108dc0d
Burke Strang <Burke.Strang@vertigis.com>
2024-06-24 12:43:17 -0400

    update scripts

---
+++ move_roslyn_dlls.ps1
@@ -0,0 +1,18 @@
+# Define the base paths
+$nugetPath = "$env:USERPROFILE\.nuget\packages\microsoft.codeanalysis.languageserver.win-x64"
+$nvimRoslynPath = "$env:USERPROFILE\AppData\Local\nvim-data\roslyn"
+
+# Delete the contents of the NVIM_ROSLYN_PATH directory
+Remove-Item -Recurse -Force "$nvimRoslynPath\*"
+
+# Find the latest version by sorting and picking the last one
+$latestVersion = Get-ChildItem -Directory $nugetPath | Sort-Object Name | Select-Object -Last 1
+
+# Check if the latest version directory exists
+if (Test-Path "$latestVersion") {
+    # Copy the contents of the latest version to the NVIM_ROSLYN_PATH directory
+    Copy-Item -Recurse -Force "$latestVersion\content\LanguageServer\win-x64\*" $nvimRoslynPath
+    Write-Output "Copied contents of version $latestVersion to $nvimRoslynPath"
+} else {
+    Write-Output "Latest version directory not found! $latestVersion"
+}
--- move_roslyn_dlls.sh
+++ move_roslyn_dlls.sh
@@ -2,7 +2,6 @@
 
 # run dotnet add package Microsoft.CodeAnalysis.LanguageServer.Linux-x64 --prerelease
 # it will error but the package will be downloaded to ~/.nuget/packages/microsoft.codeanalysis.languageserver.linux-x64
-# do the same thing but with the windows architecture to get the dlls
 
 # Define the base paths
 NUGET_PATH="$HOME/.nuget/packages/microsoft.codeanalysis.languageserver.linux-x64"
@@ -16,9 +15,9 @@ LATEST_VERSION=$(ls -v $NUGET_PATH | tail -n 1)
 
 # Check if the latest version directory exists
 if [ -d "$NUGET_PATH/$LATEST_VERSION" ]; then
-        # Copy the contents of the latest version to the NVIM_ROSLYN_PATH directory
-        cp -r "$NUGET_PATH/$LATEST_VERSION/content/LanguageServer/linux-x64/"* "$NVIM_ROSLYN_PATH/"
-        echo "Copied contents of version $LATEST_VERSION to $NVIM_ROSLYN_PATH"
+	# Copy the contents of the latest version to the NVIM_ROSLYN_PATH directory
+	cp -r "$NUGET_PATH/$LATEST_VERSION/content/LanguageServer/linux-x64/"* "$NVIM_ROSLYN_PATH/"
+	echo "Copied contents of version $LATEST_VERSION to $NVIM_ROSLYN_PATH"
 else
-        echo "Latest version directory not found!"
+	echo "Latest version directory not found!"
 fi
