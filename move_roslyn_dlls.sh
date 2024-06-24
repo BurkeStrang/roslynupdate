f4cbfde50e17b9943052f67a4e8a7df8538ca9cb
BurkeStrang <burke.strang@outlook.com>
2024-06-24 09:16:57 -0400

    update roslyn lsp

---
+++ move_roslyn_dlls.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# before you run this run dotnet-outdated
+# if there are any updates run dotnet-outdated -u
+
+# Define the base paths
+NUGET_PATH="$HOME/.nuget/packages/microsoft.codeanalysis.languageserver.linux-x64"
+NVIM_ROSLYN_PATH="$HOME/.local/share/nvim/roslyn"
+
+# Delete the contents of the NVIM_ROSLYN_PATH directory
+rm -rf "$NVIM_ROSLYN_PATH"/*
+
+# Find the latest version by sorting and picking the last one
+LATEST_VERSION=$(ls -v $NUGET_PATH | tail -n 1)
+
+# Check if the latest version directory exists
+if [ -d "$NUGET_PATH/$LATEST_VERSION" ]; then
+	# Copy the contents of the latest version to the NVIM_ROSLYN_PATH directory
+	cp -r "$NUGET_PATH/$LATEST_VERSION/content/LanguageServer/linux-x64/"* "$NVIM_ROSLYN_PATH/"
+	echo "Copied contents of version $LATEST_VERSION to $NVIM_ROSLYN_PATH"
+else
+	echo "Latest version directory not found!"
+fi
